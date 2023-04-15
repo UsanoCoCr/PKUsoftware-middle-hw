816b0bbe182882d183748ac4a006a65277417dc8
cocr <forever.kzx0713@qq.com>
2023-04-15 23:35:42 +0800

    add some throw

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,34 +8,78 @@
 #include <assert.h>
 
 AI* Parser::parse(int argc, char** argv){
-    /*for(int i=1;i<=argc;i++){
+    if (argc==1){
+        throw std::runtime_error("No parameter input");
+        return nullptr;
+    }
 
-    }*/
-    if (strcmp(argv[1],"--chat")==0){
-        chat_AI* ai=new chat_AI;
-        strcpy(ai->msg,argv[2]);
-        if (argc==3)
+    else if (argc==3){//--chat without -o or --output
+        if (strcmp(argv[1],"--chat")==0){
+            chat_AI* ai=new chat_AI;
+            strcpy(ai->msg,argv[2]);
             return ai;
-        if (strcmp(argv[3],"-o")==0 || strcmp(argv[3],"--output")==0){
-            ai->havefile=true;
-            strcpy(ai->filename,argv[4]);
         }
-        return ai;
+        else{
+            throw std::runtime_error("Parameter input error");
+            return nullptr;
+        }
     }
-    else if (strcmp(argv[1],"--draw")==0){
-        draw_AI* ai=new draw_AI();
-        ai->msg=argv[2];
-        ai->filename=argv[4];
-        return ai;
-    }
-    else if (strcmp(argv[1],"--math")==0){
-        math_AI* ai=new math_AI();
-        ai->msg=argv[2];
-        ai->filename=argv[4];
-        return ai;
+
+    else if (argc==5){
+        if (strcmp(argv[1],"--chat")==0){
+            chat_AI* ai=new chat_AI;
+            strcpy(ai->msg,argv[2]);
+            if (strcmp(argv[3],"-o")==0 || strcmp(argv[3],"--output")==0){
+                ai->havefile=true;
+                strcpy(ai->filename,argv[4]);
+            }
+            return ai;
+        }
+        else if (strcmp(argv[3],"--chat")==0){
+            chat_AI* ai=new chat_AI;
+            strcpy(ai->msg,argv[4]);
+            if (strcmp(argv[1],"-o")==0 || strcmp(argv[1],"--output")==0){
+                ai->havefile=true;
+                strcpy(ai->filename,argv[2]);
+            }
+            return ai;
+        }
+        else if (strcmp(argv[1],"--draw")==0){
+            draw_AI* ai=new draw_AI;
+            strcpy(ai->msg,argv[2]);
+            if (strcmp(argv[3],"-o")==0 || strcmp(argv[3],"--output")==0)
+                strcpy(ai->filename,argv[4]);
+            return ai;
+        }
+        else if (strcmp(argv[3],"--draw")==0){
+            draw_AI* ai=new draw_AI;
+            strcpy(ai->msg,argv[4]);
+            if (strcmp(argv[1],"-o")==0 || strcmp(argv[1],"--output")==0)
+                strcpy(ai->filename,argv[2]);
+            return ai;
+        }
+        else if (strcmp(argv[1],"--math")==0){
+            math_AI* ai=new math_AI;
+            strcpy(ai->msg,argv[2]);
+            if (strcmp(argv[3],"-o")==0 || strcmp(argv[3],"--output")==0)
+                strcpy(ai->filename,argv[4]);
+            return ai;
+        }
+        else if (strcmp(argv[3],"--math")==0){
+            math_AI* ai=new math_AI;
+            strcpy(ai->msg,argv[4]);
+            if (strcmp(argv[1],"-o")==0 || strcmp(argv[1],"--output")==0)
+                strcpy(ai->filename,argv[2]);
+            return ai;
+        }
+        else{
+            throw std::runtime_error("Parameter input error");
+            return nullptr;
+        }
     }
     else{
-        //abort();
+        throw std::runtime_error("Parameter count error");
+        return nullptr;
     }
 }
 
